5bf3101b69ffc2cd5f266543f3c2e7f4615343d5
allperdogan <47662425+allperdogan@users.noreply.github.com>
2023-11-23 00:44:04 +0300

    pages edited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BitcoinGraphComponent } from './components/bitcoin-graph/bitcoin-graph.component';
@@ -10,9 +10,10 @@ import { NaviComponent } from './components/navi/navi.component';
 import { ToastrModule } from 'ngx-toastr';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +32,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
       positionClass:"toast-bottom-right"
     }),
     ReactiveFormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgbModule,
+    NgbDropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { UserDetail } from 'src/app/models/userDetail';
+import { LocalService } from 'src/app/services/local.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  isAuthorizated = false;
+  isAnAdmin = false;
+  userDetails: UserDetail;
+
+  constructor(private localService: LocalService,
+    private router: Router,
+    private toastrService: ToastrService) { }
+
+  ngOnInit(): void {
+    this.checkIsAuthorizated()
+    this.getUserDetails()
+    this.isAdmin()
+  }
+
+
+  getUserDetails() {
+    this.userDetails = JSON.parse(this.localService.getItem("user_details") || '')
+  }
+
+  checkIsAuthorizated() {
+    if (this.localService.getItem("token")) {
+      this.isAuthorizated = true
+    } else {
+      this.isAuthorizated = false
+    }
+  }
+
+  logOut() {
+    this.localService.delete("token")
+    this.localService.delete("user_details")
+    this.localService.delete("user_claim")
+    this.toastrService.info("Çıkış yapıldı.", "Bilgilendirme!")
+    window.location.reload()
+  }
+
+  isAdmin(){
+    if (this.localService.getItem("user_claim")) {
+      this.isAnAdmin = true
+    }
+    else{
+      this.isAnAdmin = false
+    }
+  }
 }
